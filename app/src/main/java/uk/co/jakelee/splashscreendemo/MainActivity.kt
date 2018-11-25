e9b325c60b3fc00ba262c5e826bf8b4227a48721
Jake Lee <github@jakelee.co.uk>
2018-11-25 19:36:45 +0000

    All changes required for splash screen

---
+++ app/src/main/java/uk/co/jakelee/splashscreendemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package uk.co.jakelee.splashscreendemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
