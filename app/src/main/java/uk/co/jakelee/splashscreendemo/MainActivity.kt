1793390c1d1a48fa64b8a2a5ff2f11efa00df769
Jake Lee <github@jakelee.co.uk>
2018-11-25 21:09:20 +0000

    Adding delay for demo purposes

--- app/src/main/java/uk/co/jakelee/splashscreendemo/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/splashscreendemo/MainActivity.kt
@@ -1,11 +1,13 @@
 package uk.co.jakelee.splashscreendemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        sleep(3000)
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
